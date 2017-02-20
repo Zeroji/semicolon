49db6e948e85d87f8f60b805ff58ae98778f50a7
Zeroji <zzeroji@gmail.com>
2017-02-20 21:46:37 +0100

    Fixed XKCD feed file

---
+++ cogs/feed/xkcd.py
@@ -0,0 +1,25 @@
+"""XKCD feed notifier."""
+import json
+import discord
+import gearbox
+cog = gearbox.Cog()
+
+
+@cog.on_socket(b'hook/xkcd/')
+async def display_xkcd(client, data_str, socket):
+    data = json.loads(data_str)
+    for xkcd in data['items']:
+        id = xkcd['id'].split('/')[3]
+        title = xkcd['title']
+        url = xkcd['permalinkUrl']
+        summary = xkcd['summary']
+        image_url_start = summary.find('<img src="')
+        image_url_end = summary.find('" title="', image_url_start)
+        image_url = summary[image_url_start + len('<img src="'):image_url_end]
+        image_title_end = summary.find('" alt="', image_url_end)
+        alt_text = summary[image_url_end + len('" title="'):image_title_end]
+        embed = discord.Embed(title='XKCD #{id}: {title}'.format(id=id, title=title),
+                              url=url,
+                              description=alt_text)
+        embed.set_image(url=image_url)
+        await client.send_message(client.get_channel('272463984026976257'), embed=embed)
